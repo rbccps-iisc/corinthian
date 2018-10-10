7fcef6346d1cefc39e3424c5ce8af84515ae6e95
Poorna Chandra Tejasvi <pct960@gmail.com>
2018-10-10 10:26:31 +0530

    update

--- tests/require.sh
+++ tests/require.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#sudo apt update
-#sudo apt upgrade
+sudo apt update
+sudo apt upgrade
 #curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 #sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" -y
 #sudo apt-get update -y
@@ -9,6 +9,7 @@
 sudo usermod -aG docker $USER
 
 sudo apt install python python-pip libssl-dev 
+sudo pip install --upgrade cryptography
 python -m pip install requests
 python -m pip install urllib3
 #python -m pip install pyopenssl 
