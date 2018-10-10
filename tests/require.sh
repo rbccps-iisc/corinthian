bff08527d776900bade8284dec051e6382d4b0b8
Poorna Chandra Tejasvi <pct960@gmail.com>
2018-10-10 10:42:11 +0530

    update

--- tests/require.sh
+++ tests/require.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt upgrade
+#sudo apt upgrade
 #curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 #sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" -y
 #sudo apt-get update -y
@@ -11,7 +11,7 @@ sudo usermod -aG docker $USER
 #sudo apt install python python-pip libssl-dev 
 #sudo pip install -U pyopenssl
 #sudo pip install --upgrade cryptography
-#python -m pip install requests
-#python -m pip install urllib3
+sudo -H pip install requests
+sudo -H pip install urllib3
 #python -m pip install pyasn1 
 #python -m pip install ndg-httpsclient 
