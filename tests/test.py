451f73a13d9c9856f5fb35c659be9203e476701d
Poorna Chandra Tejasvi <pct960@gmail.com>
2018-10-10 19:06:42 +0530

    update

--- tests/test.py
+++ tests/test.py
@@ -14,14 +14,14 @@ from requests.packages.urllib3.poolmanager import PoolManager
 import ssl
 from functools import wraps
 
-#def sslwrap(func):
-#    @wraps(func)
-#    def bar(*args, **kw):
-#        kw['ssl_version'] = ssl.PROTOCOL_TLSv1
-#        return func(*args, **kw)
-#    return bar
+def sslwrap(func):
+    @wraps(func)
+    def bar(*args, **kw):
+        kw['ssl_version'] = ssl.PROTOCOL_TLSv1
+        return func(*args, **kw)
+    return bar
 
-#ssl.wrap_socket = sslwrap(ssl.wrap_socket)
+ssl.wrap_socket = sslwrap(ssl.wrap_socket)
 
 #class MyAdapter(HTTPAdapter):
 #    def init_poolmanager(self, connections, maxsize, block=False):
