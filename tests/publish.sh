c238e1f4915edcb5e7ddc1aba0e3fed817538c9a
Poorna Chandra Tejasvi <pct960@gmail.com>
2018-10-11 23:27:25 +0530

    update

--- tests/publish.sh
+++ tests/publish.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -ik -X POST https://127.0.0.1:8888/publish -H 'apikey: '$2'' -H 'id: '$1'' -H 'message-type: '$5'' -H 'to: '$3'' -H 'topic: '$4'' -d $6
+curl -ik -X POST https://localhost:8888/publish -H 'apikey: '$2'' -H 'id: '$1'' -H 'message-type: '$5'' -H 'to: '$3'' -H 'topic: '$4'' -d $6
--- tests/register.sh
+++ tests/register.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -ik -X POST https://127.0.0.1:8888/register -H 'Content-Type: application/json' -H 'apikey: '$2'' -H 'entity: '$3'' -H 'id: '$1'' -d '{"test":"test"}'
+curl -ik -X POST https://localhost:8888/register -H 'Content-Type: application/json' -H 'apikey: '$2'' -H 'entity: '$3'' -H 'id: '$1'' -d '{"test":"test"}'
