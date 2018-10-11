34a74022c9ffa81cd72085297a8352685deda05b
Poorna Chandra Tejasvi <pct960@gmail.com>
2018-10-11 23:20:29 +0530

    update

--- tests/require.sh
+++ tests/require.sh
@@ -2,10 +2,10 @@
 
 sudo apt update
 #sudo apt upgrade
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" -y
-sudo apt update -y
-sudo apt install docker-ce -y
+#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+#sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" -y
+#sudo apt update -y
+sudo apt install docker.io
 sudo usermod -aG docker $USER
 
 #sudo apt update 
