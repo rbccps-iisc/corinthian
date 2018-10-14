1f3f1c1b38fe8ca206fe069e5e29bd330dea7396
Poorna Chandra Tejasvi <pct960@gmail.com>
2018-10-14 09:28:26 +0530

    update

--- tests/require.sh
+++ tests/require.sh
@@ -11,5 +11,5 @@ sudo usermod -aG docker $USER
 sudo curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
-sudo python -m pip install requests
-sudo python -m pip install urllib3
+python -m pip install requests
+python -m pip install urllib3
