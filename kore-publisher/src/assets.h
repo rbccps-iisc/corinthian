415a7e112119f6f3dab4e4872f51f68c8950b023
Poorna Chandra Tejasvi <pct960@gmail.com>
2018-12-17 14:00:54 +0530

    Updated AMQP test cases (#90)

--- kore-publisher/src/assets.h
+++ kore-publisher/src/assets.h
@@ -1,14 +1,14 @@
 #ifndef __H_KORE_ASSETS_H
 #define __H_KORE_ASSETS_H
-extern const u_int8_t asset_ui_html[];
-extern const u_int32_t asset_len_ui_html;
-extern const time_t asset_mtime_ui_html;
-extern const char *asset_sha256_ui_html;
-int asset_serve_ui_html(struct http_request *);
 extern const u_int8_t asset_home_html[];
 extern const u_int32_t asset_len_home_html;
 extern const time_t asset_mtime_home_html;
 extern const char *asset_sha256_home_html;
 int asset_serve_home_html(struct http_request *);
+extern const u_int8_t asset_ui_html[];
+extern const u_int32_t asset_len_ui_html;
+extern const time_t asset_mtime_ui_html;
+extern const char *asset_sha256_ui_html;
+int asset_serve_ui_html(struct http_request *);
 
 #endif
