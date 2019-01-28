3d5608473b5ba2f9c7aabe636eb0224cefc1d468
jishnujayakumar <jishnu.jayakumar182@gmail.com>
2019-01-28 15:32:52 +0530

    UPDATE login UI

--- kore-publisher/jail/ui/assets/js/angular/login_module.js
+++ kore-publisher/jail/ui/assets/js/angular/login_module.js
@@ -4,9 +4,9 @@ login.controller('loginCtrl', function($scope, $http, origin, api){
     $scope.brand = "IUDX";
     $scope.brand_full_form = "Indian Urban Data Exchange";
     $scope.navbar_links = [
-    	{"text":"Link-1","href":"#", "legend":"planet"},
-    	{"text":"Link-2","href":"#", "legend":"compass-04"},
-    	{"text":"Link-3","href":"#", "legend":"diamond"}
+    	{"text":"Github","href":"https://github.com/rbccps-iisc/corinthian", "legend":"fab fa-github"},
+    	{"text":"Documentation","href":"https://iudx.readthedocs.io/en/latest/", "legend":"fab fa-readme"},
+    	// {"text":"Link-3","href":"#", "legend":"diamond"}
     ];
 
     $scope.data=[]
