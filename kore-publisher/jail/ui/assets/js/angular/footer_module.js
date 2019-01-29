bbed9547e6e3ef990e2cee576f2fc5e9091ab828
jishnujayakumar <jishnu.jayakumar182@gmail.com>
2019-01-29 12:27:03 +0530

    UPDATE login page with new links

--- kore-publisher/jail/ui/assets/js/angular/footer_module.js
+++ kore-publisher/jail/ui/assets/js/angular/footer_module.js
@@ -5,8 +5,8 @@ footer.controller('footerCtrl', function($scope, $http){
     $scope.copyright_owner = {"name":"RBCCPS, IISc Bangalore", "href":"http://www.rbccps.org/"};
     $scope.footer_links = [
     	{"text":"RBCCPS","href":"http://www.rbccps.org/"},
-    	{"text":"About Us","href":"#"},
-    	{"text":"Blog","href":"#"},
-    	{"text":"License","href":"#"}
+    	{"text":"About Us","href":"http://www.rbccps.org/about/"},
+    	{"text":"Contact Us","href":"mailto:iudx@rbccps.org"},
+    	// {"text":"License","href":"#"}
     ]
 });
