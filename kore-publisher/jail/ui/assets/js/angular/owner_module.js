6e5a2224ac044df74c3a97d91a97af0bc815caad
jishnujayakumar <jishnu.jayakumar182@gmail.com>
2019-01-30 15:19:34 +0530

    UI release 1.0

--- kore-publisher/jail/ui/assets/js/angular/owner_module.js
+++ kore-publisher/jail/ui/assets/js/angular/owner_module.js
@@ -172,4 +172,85 @@ owner.controller('ownerCtrl', function($scope, $compile, $http){
                     }, 900);
             });
     }
+
+
+    // Follow Entity
+    $scope.follow=function(){
+      //console.log($scope.is_autonomous==undefined)
+      // console.log($scope.is_autonomous)
+      var is_autonomous=($scope.is_autonomous==undefined || $scope.is_autonomous==false)?false:true;
+      $http({
+          method: 'POST',
+          url: api['owner']['register-entity'],
+          headers: {
+              'id': $scope.id,
+              'apikey': $scope.apikey,
+              'entity': $scope.entity_name,
+              'is-autonomous':is_autonomous
+          },
+          data: {} 
+      }).then(function (response)
+            {
+
+                  function checker(flag){
+                    if(flag){
+                      return 'checked'
+                    }else{
+                      return ''
+                    }
+                  }
+
+                  function copyToClipboard(element_id) {
+                      var $temp = $("<input>");
+                      $("body").append($temp);
+                      $temp.val($("#new_apikey_"+element_id).text()).select();
+                      $("#copied_"+element_id).html("APIKEY copied!")
+                      document.execCommand("copy");
+                      $temp.remove();
+                  }
+                    
+                    var data=JSON.parse(localStorage.getItem('data'));
+                    var _obj = {'ent':$scope.id+"/"+$scope.entity_name, 'is_blocked':false, 'is_autonomous':is_autonomous, 'index': $scope.id+"_"+$scope.entity_name}
+                    data.push(_obj);
+                    localStorage.setItem('data', JSON.stringify(data));
+                    // console.log(JSON.parse(localStorage.getItem('data')))
+                    
+
+                  var compiled_entity_row=$compile(entity_row)($scope);
+                    $("#entity_list").each(function() {
+                        if ($(this).html()){
+                            $(this).prepend(compiled_entity_row);
+                        }else{
+                            $(this).append(compiled_entity_row);
+                        }
+                    });
+
+                    // $("cb_"+ _obj['index']).prop('checked', _obj['is_autonomous'])
+                    $("#alert_message").html(`<br><div class="alert alert-success alert-dismissible fade show in" role="alert">
+                            <span class="alert-inner--icon"><i class="ni ni-like-2"></i></span>
+                            <span class="alert-inner--text"><strong>Success! </strong> <strong>` + response.data.id + `</strong> has been registered with apikey <strong id="new_apikey_`+ _obj['own'] +`">`+ response.data.apikey +`</strong> <strong id="copied_`+ _obj['own'] +`" onclick="copyToClipboard('`+_obj['own']+`')"><i style="font-size:30px;" class="far fa-copy"></i></strong></span>
+                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>`);
+                    window.setTimeout(function(){
+                      $( "#alert_message").fadeIn();
+                      $( "#alert_message").fadeOut(3050);
+                    }, 900);
+                  
+
+            }, function(error){
+               $( "#alert_message").html(`<br><div class="alert alert-danger alert-dismissible fade show" role="alert">
+                            <span class="alert-inner--icon"><i class="fas fa-exclamation-triangle"></i></span>
+                            <span class="alert-inner--text"><strong>Error! </strong>` + error['data']['error'] + `</span>
+                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>`);
+               window.setTimeout(function(){
+                      $( "#alert_message").fadeIn();
+                      $( "#alert_message").fadeOut(450);
+                    }, 900);
+            });
+    }
 });
