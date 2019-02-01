016d258de1655c43de47b9a3d78b059d32037fed
arun <arun.hbni@gmail.com>
2019-02-01 11:33:07 +0530

    [skip travis] update

--- setup/kore/kore_install.sh
+++ setup/kore/kore_install.sh
@@ -22,6 +22,7 @@ head -c1024 < /dev/urandom > jail-keymgr/random.data
 
 chown kore_keymgr:kore_keymgr jail-keymgr 
 chown kore_keymgr:kore_keymgr jail-keymgr/random.data
+chmod 600 jail-keymgr 
 chmod 600 jail-keymgr/random.data
 
 chmod u+rw jail-keymgr/random.data
