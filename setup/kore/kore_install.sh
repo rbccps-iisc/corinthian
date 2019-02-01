796a3af5439bcc60b737a390ce7bed1c6e8b60c8
arun <arun.hbni@gmail.com>
2019-02-01 11:41:50 +0530

    [skip travis] readonly random.data is ok

--- setup/kore/kore_install.sh
+++ setup/kore/kore_install.sh
@@ -22,9 +22,10 @@ head -c1024 < /dev/urandom > jail-keymgr/random.data
 
 chown kore_keymgr:kore_keymgr jail-keymgr 
 chown kore_keymgr:kore_keymgr jail-keymgr/random.data
-chmod 600 jail-keymgr 
-chmod 600 jail-keymgr/random.data
 
-chmod u+rw jail-keymgr/random.data
+chmod 555 jail-keymgr 
+
+# let no one write to random.data
+chmod 444 jail-keymgr/random.data
 
 tmux new-session -d -s kore 'cd /kore-publisher && kodev build && kore -fc conf/kore-publisher.conf'
